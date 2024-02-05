b9cde65fb3644121190edc0eddf8eb433f9ceb41
nhardiansa <nhardiansa@gmail.com>
2024-02-05 17:26:40 +0700

    feat: add core, auth, user feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CoreModule } from './core/core.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [CoreModule, AuthModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
