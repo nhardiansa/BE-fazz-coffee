ed542c7e052eec62d7cebb094a5ae179e23be701
nhardiansa <nhardiansa@gmail.com>
2024-02-06 01:20:33 +0700

    fix: userId must be nullable

--- prisma/migrations/20240205171344_nullable_user_id_relation/migration.sql
+++ prisma/migrations/20240205171344_nullable_user_id_relation/migration.sql
@@ -4,7 +4,6 @@
   - You are about to drop the column `userId` on the `auth` table. All the data in the column will be lost.
   - A unique constraint covering the columns `[user_id]` on the table `auth` will be added. If there are existing duplicate values, this will fail.
   - Added the required column `updated_at` to the `auth` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `user_id` to the `auth` table without a default value. This is not possible if the table is not empty.
   - Added the required column `updated_at` to the `user` table without a default value. This is not possible if the table is not empty.
 
 */
@@ -15,7 +14,7 @@ ALTER TABLE `auth` DROP FOREIGN KEY `Auth_userId_fkey`;
 ALTER TABLE `auth` DROP COLUMN `userId`,
     ADD COLUMN `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     ADD COLUMN `updated_at` DATETIME(3) NOT NULL,
-    ADD COLUMN `user_id` INTEGER NOT NULL;
+    ADD COLUMN `user_id` INTEGER NULL;
 
 -- AlterTable
 ALTER TABLE `user` ADD COLUMN `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
@@ -32,4 +31,4 @@ ALTER TABLE `user` ADD COLUMN `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_
 CREATE UNIQUE INDEX `Auth_userId_key` ON `auth`(`user_id`);
 
 -- AddForeignKey
-ALTER TABLE `auth` ADD CONSTRAINT `Auth_userId_fkey` FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `auth` ADD CONSTRAINT `Auth_userId_fkey` FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
