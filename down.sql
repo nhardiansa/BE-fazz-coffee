0c76f540b3b9ae19db976161521e3f376a67a4e4
nhardiansa <nhardiansa@gmail.com>
2024-02-05 23:10:53 +0700

    fix: change standard

--- down.sql
+++ down.sql
@@ -1,26 +1,37 @@
--- CreateTable
-CREATE TABLE `post` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `title` VARCHAR(191) NOT NULL,
-    `content` VARCHAR(191) NULL,
-    `published` BOOLEAN NULL DEFAULT false,
-    `authorId` INTEGER NULL,
+-- DropForeignKey
+ALTER TABLE `Auth` DROP FOREIGN KEY `Auth_userId_fkey`;
 
-    INDEX `Post_authorId_fkey`(`authorId` ASC),
+-- DropTable
+DROP TABLE `User`;
+
+-- DropTable
+DROP TABLE `Auth`;
+
+-- CreateTable
+CREATE TABLE `auth` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `userId` INTEGER NOT NULL,
+
+    UNIQUE INDEX `Auth_email_key`(`email` ASC),
+    UNIQUE INDEX `Auth_userId_key`(`userId` ASC),
     PRIMARY KEY (`id` ASC)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `user` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `email` VARCHAR(191) NOT NULL,
-    `password` VARCHAR(191) NOT NULL,
-    `name` VARCHAR(191) NULL,
+    `first_name` VARCHAR(191) NOT NULL,
+    `last_name` VARCHAR(191) NOT NULL,
+    `display_name` VARCHAR(191) NOT NULL,
+    `birthdate` VARCHAR(191) NOT NULL,
+    `delivery_address` VARCHAR(191) NOT NULL,
+    `phone_number` VARCHAR(191) NOT NULL,
 
-    UNIQUE INDEX `User_email_key`(`email` ASC),
     PRIMARY KEY (`id` ASC)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `post` ADD CONSTRAINT `Post_authorId_fkey` FOREIGN KEY (`authorId`) REFERENCES `user`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `auth` ADD CONSTRAINT `Auth_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `user`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
---
+++ prisma/migrations/20240205160946_change_to_another_standard/migration.sql
@@ -0,0 +1,35 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `userId` on the `auth` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[user_id]` on the table `auth` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `updated_at` to the `auth` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `user_id` to the `auth` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updated_at` to the `user` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE `auth` DROP FOREIGN KEY `Auth_userId_fkey`;
+
+-- AlterTable
+ALTER TABLE `auth` DROP COLUMN `userId`,
+    ADD COLUMN `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    ADD COLUMN `updated_at` DATETIME(3) NOT NULL,
+    ADD COLUMN `user_id` INTEGER NOT NULL;
+
+-- AlterTable
+ALTER TABLE `user` ADD COLUMN `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    ADD COLUMN `profile_image` VARCHAR(191) NULL,
+    ADD COLUMN `updated_at` DATETIME(3) NOT NULL,
+    MODIFY `first_name` VARCHAR(191) NULL,
+    MODIFY `last_name` VARCHAR(191) NULL,
+    MODIFY `display_name` VARCHAR(191) NULL,
+    MODIFY `birthdate` VARCHAR(191) NULL,
+    MODIFY `delivery_address` VARCHAR(191) NULL,
+    MODIFY `phone_number` VARCHAR(191) NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX `Auth_userId_key` ON `auth`(`user_id`);
+
+-- AddForeignKey
+ALTER TABLE `auth` ADD CONSTRAINT `Auth_userId_fkey` FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
