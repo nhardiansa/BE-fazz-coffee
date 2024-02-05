06b184aacb0418d8d5eb8f28d20dace30db8f646
nhardiansa <nhardiansa@gmail.com>
2024-02-05 21:13:52 +0700

    fix: move core service into core folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CoreModule } from './core/core.service';
+import { CoreModule } from './core/services/core.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
