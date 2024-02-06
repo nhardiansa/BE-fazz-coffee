604a306de5e2bba448bfc460770216118385c558
nhardiansa <nhardiansa@gmail.com>
2024-02-06 21:20:37 +0700

    feat: add jwt and configure

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,18 @@ import { AppService } from './app.service';
 import { CoreModule } from './core/services/core.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-
+import { JwtModule } from '@nestjs/jwt';
 @Module({
-  imports: [CoreModule, AuthModule, AuthModule, UsersModule],
+  imports: [
+    CoreModule,
+    AuthModule,
+    AuthModule,
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: process.env.SECRET_KEY,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
