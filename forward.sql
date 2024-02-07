f5cede2daa3b06706ca593538ba8ff58a1538f58
nhardiansa <nhardiansa@gmail.com>
2024-02-07 10:23:09 +0700

    feat: add verify and reset table to insert token

--- forward.sql
+++ forward.sql
@@ -1,4 +1,16 @@
--- AlterTable
-ALTER TABLE `auths` ADD COLUMN `is_verified` BOOLEAN NULL DEFAULT false,
-    ADD COLUMN `role` ENUM('ADMIN', 'CUSTOMER') NULL DEFAULT 'CUSTOMER';
+-- CreateTable
+CREATE TABLE `verify_reset_tokens` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `user_id` INTEGER NULL,
+    `token` VARCHAR(191) NOT NULL,
+    `token_type` ENUM('VERIFY', 'RESET') NOT NULL,
+    `expired_at` DATETIME(3) NOT NULL,
+    `created_at` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `verify_reset_tokens` ADD CONSTRAINT `verify_reset_tokens_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
