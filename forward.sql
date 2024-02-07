5fe3ca0a30ef252d2a03026e4d6fd9bd67d84a8d
nhardiansa <nhardiansa@gmail.com>
2024-02-07 13:58:29 +0700

    fix: make user id in verify reset token table to non null default

--- forward.sql
+++ forward.sql
@@ -1,16 +1,9 @@
--- CreateTable
-CREATE TABLE `verify_reset_tokens` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `user_id` INTEGER NULL,
-    `token` VARCHAR(191) NOT NULL,
-    `token_type` ENUM('VERIFY', 'RESET') NOT NULL,
-    `expired_at` DATETIME(3) NOT NULL,
-    `created_at` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `updated_at` DATETIME(3) NULL,
+-- DropForeignKey
+ALTER TABLE `verify_reset_tokens` DROP FOREIGN KEY `verify_reset_tokens_user_id_fkey`;
 
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- AlterTable
+ALTER TABLE `verify_reset_tokens` MODIFY `user_id` INTEGER NOT NULL;
 
 -- AddForeignKey
-ALTER TABLE `verify_reset_tokens` ADD CONSTRAINT `verify_reset_tokens_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `verify_reset_tokens` ADD CONSTRAINT `verify_reset_tokens_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
